11f03bc987fdcfc2119fa7423c2a97ce7adf124d
Rama Sheren <ramasheren2004@gmail.com>
2025-07-20 12:58:54 +0300

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import list_books  # ✅ Needed for check
+from .views import LibraryDetailView  # Also needed if you're linking class-based view
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
